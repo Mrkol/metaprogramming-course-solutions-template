5a9ccb22cf2dfd42be935daf8fe0bf7553d6c48c
mchlkrpch <143273709+mchlkrpch@users.noreply.github.com>
2024-11-03 17:57:56 +0300

    Update PolymorphicMapper.hpp

--- mapper/PolymorphicMapper.hpp
+++ mapper/PolymorphicMapper.hpp
@@ -2,11 +2,53 @@
 
 #include <optional>
 
-
-template <class From, auto target>
+template
+  < class From
+  , auto target >
 struct Mapping;
 
-template <class Base, class Target, class... Mappings>
-struct PolymorphicMapper {
-  static std::optional<Target> map(const Base& object);
+template
+  < class Base
+  , class Target
+  , class... Mappings >
+struct PolymorphicMapper;
+
+template
+  < class Base
+  , class Target
+  , template < class From, auto target> class Mapping
+  , class From
+  , auto target
+  , class... RemainMappings >
+// Consider next mapping<from, target> from the pack
+struct PolymorphicMapper
+  < Base
+  , Target
+  , Mapping<From, target>
+  , RemainMappings... >
+{
+  static std::optional<Target> map(const Base& object) {
+    // Check if it's not From or inheritance of the From typename and if it's not derived
+    if (dynamic_cast<const From*>(&object) == nullptr || !std::derived_from<From, Base>) {
+      // The check next mapping
+      return PolymorphicMapper<Base, Target, RemainMappings...>::map(object);
+    } else {
+      // Else return target
+      return target;
+    }
+  }
+};
+
+template
+  < class Base
+  , class Target >
+struct PolymorphicMapper
+  < Base
+  , Target>
+{
+  // If we pass all arguments from the pack, then we unforetunately didn't reach the mapping
+  // and we return nullopt
+  static std::optional<Target> map(const Base& object __attribute__((unused))){
+    return std::nullopt;
+  }
 };
